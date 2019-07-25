588f862af396d99b503fca14718dec6a4430c273
nico2236 <nico2236@gmail.com>
2019-07-24 21:48:28 -0300

    ejercicio19

---
+++ Ejercicio19.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int multiplosencontrados=0;
+    int actual=1;
+    int basta;
+    printf("Elija Un Numero: ");
+    scanf("%d",&basta);
+    while (multiplosencontrados != basta){
+        if (actual%3 == 0){
+            if (actual%5 != 0){
+                multiplosencontrados = multiplosencontrados+1;
+                printf("Hay %d Multiplos Encontrados Y El Multiplo Es: %d\n", multiplosencontrados , actual);
+               
+            }
+        }
+        actual = actual+1;
+    }
+}
