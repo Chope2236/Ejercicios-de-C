6a7a65c60ef24ac41abad428e8e7b62a3cfa3de1
Chope2236 <chopetrumpis@gmail.com>
2019-07-25 14:05:06 -0300

    ejercicio1

---
+++ ejercicio1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int a , b;
+    printf ("Elija Un Numero A:\n");
+    scanf ("%d",&a);
+    printf ("Elija Un Numero B:\n");
+    scanf ("%d",&b);
+    printf ("A+B Es=%d\n",a+b);
+    printf ("A-B Es=%d\n",a-b);
+    printf ("A*B Es=%d\n",a*b);
+}
