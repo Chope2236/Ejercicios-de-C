5fd187def652117e50459105e6344181cef855e2
Sebastian <Chopetrumpis@gmail.com>
2020-03-24 17:02:20 -0300

    ejercicio12else

---
+++ ejercicio12else.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int edad;
+    printf("Ingrese la edad del socio:\n");
+    scanf("%d", &edad);
+    if (edad <= 12){
+        printf("El socio es Menor\n");
+    }else
+    {
+        if((edad >= 13) && (edad <= 18)){
+            printf("El socio es Cadete\n");
+        }else
+        {
+            if((edad >= 19) && (edad <= 26)){
+                printf("El socio es Juvenil\n");
+            }else
+            {
+                if (edad >= 27){
+                    printf("El socio es Mayor\n");
+                }
+            }
+            
+        }
+        
+    } 
+  system("pause");
+}
