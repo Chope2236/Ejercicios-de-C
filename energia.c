37684feb02f86bfce7742e19c18f3991a1c993d4
Sebastian <Chopetrumpis@gmail.com>
2021-04-19 21:12:13 -0300

    se agrego el ejercicio y se cambio un concepto

--- energia.c
+++ energia.c
@@ -6,12 +6,15 @@ int main(){
     printf("Antes que nada, definamos unos conceptos basicos\n");
     printf("Trabajo = Fuerza*Distancia\n");
     printf("Potencia = Trabajo/tiempo\n");
-    printf("1 Watt = 745 HP\n\n\n\n\n\n");
+    printf("1 HP = 745 Watts\n\n\n\n\n\n");
     printf("1. Calcular potencia\n");
     menu = getch();
     switch (menu)
     {
     case '1':
+
+    /* El ejercicio era "Se deben subir a 40m de altura 400 litros de agua, esto lo hace una bomba en 60 segundos,
+     ¿cual es la potencia de la bomba?" */
         printf("Ingrese Fuerza en Kilogramos: \n");
         scanf("%d", &fuerza);
         printf("Ingrese Distancia en Metros:\n");
