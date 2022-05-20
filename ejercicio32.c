4da34556c72ec0a5a73304a01c89b6dda92e46f4
unknown <Chopetrumpis@gmail.com>
2022-05-19 22:21:57 -0300

    Ejercicio32

---
+++ ejercicio32.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, z = 1, x = 0, y = 1;
+    scanf("%d", &n);
+    for (int i = 0; i < n-1; i++)
+    {
+        z = x + y;
+        x = y;
+        y = z;
+    }
+    printf("%d", z);
+}
